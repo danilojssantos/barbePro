4b8a376d68c95b7cc5a88c1dcb74d21a5858f925
danilojssantos <danilojssantos1@gmail.com>
2024-04-11 20:19:59 -0300

    createHaircutService

---
+++ src/services/haircut/CreateHaircutService.ts
@@ -0,0 +1,25 @@
+import prismaClient from "../../prisma";
+interface HairCutRequest{
+    user_id: string;
+    name: string;
+    price: number;
+}
+class CreateHaircutService{
+    async execute({user_id, name, price}:HairCutRequest){
+
+        if (!name || !price) {
+            throw new Error("Error")
+        }
+
+        const haircut = await prismaClient.haircut.create({
+            data:{
+                name: name,
+                price:price,
+                user_id: user_id
+            }
+        })
+
+    }
+
+}
+export {CreateHaircutService}
